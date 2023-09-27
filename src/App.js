82756f7204b0035963c4b0928ae4e141c720920d
AndreGallini <andrea.gallini01@libero.it>
2023-09-27 10:20:04 +0200

    add multilang

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import Layout from "./components/Layout";
 import { useStoryblok, StoryblokComponent } from "@storyblok/react";
 
 function App() {
-  const slug =
-  window.location.pathname === "/"
-    ? "home"
-    : window.location.pathname.replace("/", "");
+  let languages = ["en"]; //any languages you have in storyblok
+  let slug =
+    window.location.pathname === "/"
+      ? "home"
+      : window.location.pathname.replace("/", "");
+  //convert current url to array
+  let slugArray = slug.split("/");
+  let firstItem = slugArray[0];
+  //Checking if the url containes any language
+  let isLang = languages.includes(firstItem);
+  //if dose we are going to use that or fall back to default "en".
+  //In case you have a diffrent default language you can use that lang code
+  let language = isLang ? firstItem : "it";
+
+  //Now the logic for filtering out the language from url
+  //This is what we will use for the slug in "useStoryblok"
+  let stSlug =
+    isLang && slugArray.length === 1
+      ? "home"
+      : isLang
+      ? slugArray?.filter((_, i) => i !== 0)?.join("/")
+      : slugArray.join("/");
+
+  //We are also going to pass the language as prop to get the correct data based on the language
 
 const story = useStoryblok(
-  slug,
+  stSlug, { version: "draft", language },
   {
     resolve_relations: ["featured-posts.posts", "selected-posts.posts"],
   },
