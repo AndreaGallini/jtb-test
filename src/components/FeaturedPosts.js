7b5ff2091871acaecd91b929e3850eff61df0449
AndreGallini <andrea.gallini01@libero.it>
2023-09-27 10:40:09 +0200

    add log

--- src/components/FeaturedPosts.js
+++ src/components/FeaturedPosts.js
@@ -1,14 +1,15 @@
 import { storyblokEditable } from "@storyblok/react";
 
 const FeaturedPosts = ({ blok }) => {
+  console.log(blok);
   return (
     <div
-      className="py-8 mb-6 container mx-auto text-left"
+      className="container py-8 mx-auto mb-6 text-left"
       key={blok._uid}
       {...storyblokEditable(blok)}
     >
       <div className="relative">
-        <h2 className="relative font-serif text-4xl z-10 text-primary">
+        <h2 className="relative z-10 font-serif text-4xl text-primary">
           {blok.title}
         </h2>
         <div className="absolute top-0 w-64 h-10 mt-6 -ml-4 bg-yellow-300 opacity-50" />
@@ -16,14 +17,14 @@ const FeaturedPosts = ({ blok }) => {
       <ul className="flex">
         {blok.posts.map((post) => {
           return (
-            <li key={post.slug} className="pr-8 w-1/3">
+            <li key={post.slug} className="w-1/3 pr-8">
               <a
                 href={`/blog/${post.slug}`}
-                className="py-16 block transition hover:opacity-50"
+                className="block py-16 transition hover:opacity-50"
               >
-                <img src={post.content.image} className="pb-10 w-full" />
+                <img src={post.content.image} className="w-full pb-10" />
                 <h2 className="pb-6 text-lg font-bold">{post.content.title}</h2>
-                <p className="pb-6 text-gray-700 leading-loose">
+                <p className="pb-6 leading-loose text-gray-700">
                   {post.content.intro}
                 </p>
               </a>
