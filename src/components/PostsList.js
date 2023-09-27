220bb46d54458f75a4045f9f57b931abf8525bee
AndreGallini <andrea.gallini01@libero.it>
2023-09-27 10:26:38 +0200

    try fix

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,19 +1,21 @@
 import { storyblokEditable } from "@storyblok/react";
 
 const PostLists = ({ blok }) => {
+  console.log(blok.posts)
   return (
     <ul
-      className="mx-auto w-full flex flex-col items-center"
+      className="flex flex-col items-center w-full mx-auto"
       {...storyblokEditable(blok)}
       key={blok._uid}
     >
       {blok.posts.map((post) => {
+        console.log(post);
         return (
           <li
             key={post.slug}
-            className="max-w-4xl w-full px-10 my-4 py-6 rounded-lg shadow-md bg-white"
+            className="w-full max-w-4xl px-10 py-6 my-4 bg-white rounded-lg shadow-md"
           >
-            <div className="flex justify-between items-center">
+            <div className="flex items-center justify-between">
               <span className="font-light text-gray-600">
                 {`
                     ${new Date(post.created_at).getDay()}.
@@ -23,14 +25,14 @@ const PostLists = ({ blok }) => {
             </div>
             <div className="mt-2">
               <a
-                className="text-2xl text-gray-700 font-bold hover:text-gray-600"
+                className="text-2xl font-bold text-gray-700 hover:text-gray-600"
                 href={`/blog/${post.slug}`}
               >
                 {post.content.title}
               </a>
               <p className="mt-2 text-gray-600">{post.content.intro}</p>
             </div>
-            <div className="flex justify-between items-center mt-4">
+            <div className="flex items-center justify-between mt-4">
               <a
                 className="text-blue-600 hover:underline"
                 href={`/blog/${post.slug}`}
